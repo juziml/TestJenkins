a59d9d6942f2b805c60f2a1180c10fa97dab6c5b
HarkBen <sink50@foxmail.com>
2017-02-06 14:50:59 +0800

    0---

---
+++ app/src/main/java/zsw/testjenkins/JenkinsActivity.java
@@ -0,0 +1,25 @@
+package zsw.testjenkins;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+/**
+ * Create on 2017/2/6.
+ * github  https://github.com/HarkBen
+ * Description:
+ * -----------
+ * author Ben
+ * Last_Update - 2017/2/6
+ */
+public class JenkinsActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TextView tv = new TextView(this);
+        tv.setText("TestJenkins-");
+        tv.setTextSize(30.0f);
+        setContentView(tv);
+    }
+}
